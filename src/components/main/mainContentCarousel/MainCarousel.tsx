3ac09b6684a5c425c2e8e0cd08ba27fb8bf04a28
Nomad-lab-sketch <52406767+Nomad-lab-sketch@users.noreply.github.com>
2022-05-05 19:29:59 +0300

    add_style_crousel

--- src/components/main/mainContentCarousel/MainCarousel.tsx
+++ src/components/main/mainContentCarousel/MainCarousel.tsx
@@ -7,6 +7,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import { GoArrowLeft, GoArrowRight } from "react-icons/go";
+import { MdFavoriteBorder, MdOutlineShoppingBasket } from "react-icons/md";
+import { IoStarSharp } from "react-icons/io5";
 
 import s from "./css/style.module.css";
 import Slider from "react-slick";
@@ -32,24 +34,55 @@ const MainContent = () => {
 
   const settings = {
     infinite: true,
-    slidesToShow: 3,
-    speed: 700,
-    dots: true,
+    slidesToShow: 4,
+    speed: 800,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
   };
 
+  const ratingMatch = () => {
+    return (
+      <>
+        <IoStarSharp className={s.rating__star} style={{ color: "#e9a426" }} />
+        <IoStarSharp className={s.rating__star} style={{ color: "#e9a426" }} />
+        <IoStarSharp className={s.rating__star} style={{ color: "#e9a426" }} />
+        <IoStarSharp className={s.rating__star} style={{ color: "#e9a426" }} />
+        <IoStarSharp className={s.rating__star} style={{ color: "#e9a426" }} />
+      </>
+    );
+  };
+
   return (
     <div className={s.mainCarouselWrapper}>
-      <Slider {...settings}>
-        {product.map((i) => (
-          <div key={i.id} className={s.container__productCard}>
-            <div className={s.productCard__name}>Name: {i.name}</div>
-            <div className={s.productCard__count}>Viewers: {i.count}</div>
-            <div className={s.productCard__price}>Price: {i.price}</div>
-          </div>
-        ))}
-      </Slider>
+      <div className={s.mainCarouselWrapper__container}>
+        <Slider {...settings}>
+          {product.map((i) => (
+            <div key={i.id} className={s.container__productCard}>
+              <div className={s.container__productCard__wrapper}>
+                <div className={s.container__productCard__title}>
+                  <div className={s.container__productCard__title__name}>{i.name}</div>
+                </div>
+                <div className={s.container__productCard__reviews}>
+                  <div className={s.container__productCard__reviews__count}>
+                    {ratingMatch()} <span>{i.count}</span>
+                  </div>
+                </div>
+                <div className={s.container__productCard__buy}>
+                  <div className={s.container__productCard__buy__price}>{i.price} RUB</div>
+                  <div className={s.container__productCard__buy__button}>
+                    <div className={s.container__productCard__buy__button__buttonAddFavorite}>
+                      <MdFavoriteBorder />
+                    </div>
+                    <div className={s.container__productCard__buy__button__buttonBasket}>
+                      <MdOutlineShoppingBasket />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
